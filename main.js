22b54e0a8154d17bf4f46604bcd79615d5d733fe
Messi_G <negash4@gmail.com>
2021-08-27 23:09:18 -0500

    reduced method working

--- main.js
+++ main.js
@@ -225,13 +225,20 @@ console.log(firstCheapCandy)
             </section>
             `;
         });
+// Use the reduce method on the following array to determine how much total rain fell last month.
+const monthlyRainfall = [23, 13, 27, 20, 20, 31, 33, 26, 19, 12, 14, 12, 10]
 
+const totalRainfall = monthlyRainfall.reduce((totalAsOfNow, nextValue) => totalAsOfNow+= nextValue, 0)
+
+console.log(totalRainfall)
+
+// Use the reduce method on the following array to build a sentence.
        
-   
+const words = ["The", "quick", "brown", "fox", "jumped", "over", "the", "lazy", "dog"]
 
+const sentence = words.reduce((firstString, nextString) => firstString+=nextString, "")
 
-    
-
+console.log(sentence)
 
 const init = () => {
           
